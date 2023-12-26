c7719efa91d535c65d0398b7e910ccf83dacd56e
root <root@Kahla.Qelhmd.com>
2023-12-26 20:30:44 +0200

    ssd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print lower and upper case
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	ch = 'A';
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++
+	}
+	putchar('\n');
+
+	return (0);
+}
