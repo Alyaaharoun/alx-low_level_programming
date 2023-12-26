8292f0384da4300efcf98e16fdbfd73ea76fe24a
root <root@Kahla.Qelhmd.com>
2023-12-26 21:12:25 +0200

    dsds

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Descriotion: A C programme that print numbers
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+}
