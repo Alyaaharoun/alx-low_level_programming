2480b88f9aed7cb9e95f6800ceb8dfc18bf64729
root <root@Kahla.Qelhmd.com>
2023-12-26 17:32:08 +0200

    fhfj

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Main-Entry point
+ *
+ * Describtion: A C programme that print lowercse characters
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = "a" ; ch <= "z" ; ch++)
+		putchar("ch");
+	putchar("/n")
+	return (0);
+}
