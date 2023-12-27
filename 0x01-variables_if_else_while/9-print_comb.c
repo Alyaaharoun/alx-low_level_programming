b8fe7df3edb19992fcbaa544e6593f6fad58ceb4
root <root@Kahla.Qelhmd.com>
2023-12-27 12:15:37 +0200

    sds

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C programme that print aseparated numbers
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		putchar(' ');
+		putchar(',');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
