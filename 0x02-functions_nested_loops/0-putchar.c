619cd14b037be3bc9d373e97398a4d3b480fbe9b
root <root@Kahla.Qelhmd.com>
2023-12-31 16:14:48 +0200

    printf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print string
+ *
+ *Return : Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar");
+	printf("\n");
+
+	return (0);
+}
