580df5d030c67eec0849655c0c3be0d5238b430c
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-05 17:52:23 +0200

    dgsfd

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,19 +3,23 @@
 /**
  * _abs - function tht print the bsolute of integer
  *
+ * @n: parameter of function
+ *
  * Return: always 0 (success)
  *
  */
 
-int _abs(int)
+int _abs(int n)
 {
-	int n;
+	int i;
+
+	i = n * 1
 
 	if (n > 0)
 		_putchar(n);
 	else if (n == 0)
 		_putchar('0');
 	else if (n < 0)
-		_putchr(n * -1);
+		_putchr(i);
 	return (0);
 }
