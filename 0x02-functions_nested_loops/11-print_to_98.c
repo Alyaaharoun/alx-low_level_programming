57f2ed3ec54142e5330564a98c3381e067b58fd3
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-06 21:06:12 +0200

    eeew

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 			printf(",");
-			printf("");
+			printf(" ");
 		}
 	}
 
@@ -26,7 +26,7 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 			printf(",");
-			printf("");
+			printf(" ");
 		}
 	}
 }
