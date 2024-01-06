2bc4f0ca4dd32ec78f20fd8430727155eae508ab
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-07 01:36:08 +0200

    wwew

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - function print numbers ten times
+ *
+ * Return: always 0 (success)
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (i = 0; i < 15; i++)
+			_putchar(i + 48);
+	}
+	_putchar('\n');
+}
