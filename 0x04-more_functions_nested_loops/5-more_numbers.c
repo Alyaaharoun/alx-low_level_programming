c555b871adf70636c579f8eda9a512915d094e2b
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-07 01:56:30 +0200

    sss

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,21 @@ void more_numbers(void)
 {
 	int i;
 	int n;
+	int c;
+
 
 	for (n = 1; n <= 10; n++)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			_putchar(i + 48);
-			_putchar('\n');
+			i = c;
+			if (c > 9)
+			{
+				_putchar(1 + 48);
+				c = n % 10;
+			}
+			_putchar(c + 48);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
