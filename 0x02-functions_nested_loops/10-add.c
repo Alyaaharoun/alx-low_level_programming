5e6c2cae739092285a3b46ecacb81d5ab238d202
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-06 20:34:27 +0200

    qqqe

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - function that print the add of two integer
+ *
+ * @a: first parameter
+ *
+ * @b: second parameter
+ *
+ * Return: always 0 (success)
+ */
+
+int add(int a, int b)
+{
+	int d;
+
+	d = a + b;
+	return (d);
+}
