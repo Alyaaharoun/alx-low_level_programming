9046cc60715c4559f0eac47e59ecf8d70bf2eb38
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-06 20:27:52 +0200

    eerrr

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,11 +12,11 @@ void times_table(void)
 	int j;
 	int r;
 
-	for (i = 1; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar('0');
 
-		for (j = 0; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
 			r = i * j;
 
