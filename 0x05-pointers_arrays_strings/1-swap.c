2d6afc4e14c2e996dc54d16c5c63a733b18806c5
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-08 04:07:06 +0200

    aaa

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swap two number
+ *
+ * @a: first parameter as first number
+ *
+ * @b: second parameter as second number
+ *
+ * Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+	swap(*a, *b);
+}
