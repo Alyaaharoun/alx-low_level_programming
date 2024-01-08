1548dd8a7c7437e2475f8b7a0c6d3424eddd8c19
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-08 03:25:40 +0200

    sss

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to change the number to another
+ *
+ * @*n: aparameter of function
+ *
+ * Return: always 0(success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
