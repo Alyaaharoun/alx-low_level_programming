34be3fdfac90b880297af65af82cc43cf7fc73dc
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-08 04:19:38 +0200

    sss

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - function thatprint the legnth
+ *
+ * @s: parameter of function
+ *
+ * Return: always 0 (success)
+ */
+
+int _strlen(char *s)
+{
+	strlen(*s);
+}
