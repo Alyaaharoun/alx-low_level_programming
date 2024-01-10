7b2e89f02ddf1bbe0130151a671c264e4afb8959
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-11 01:28:53 +0200

    put

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that print string
+ *
+ * @s: parameter of function
+ *
+ * Return: no return
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+}
