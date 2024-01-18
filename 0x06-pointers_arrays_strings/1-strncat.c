1f6c8ae31888faa3829996a540e49510b07929eb
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-18 13:40:24 +0200

    ssww

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strncat - function to print number of character to another
+ *
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destl = 0;
+	int srcl = 0;
+	int i;
+
+	while (dest[destl] != '\0')
+	{
+		destl++;
+	}
+	while (src[srcl] != '\0')
+	{
+		srcl++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[destl + i] = src[i];
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -3,17 +3,22 @@
 #include <string.h>
 
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 int main(void)
 {
+	
 	char s1[98] = "Hello ";
     char s2[] = "World!\n";
     char *ptr;
 
     printf("%s\n", s1);
     printf("%s", s2);
-    ptr = _strcat(s1, s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
     printf("%s", s1);
     printf("%s", s2);
     printf("%s", ptr);
@@ -25,21 +30,21 @@ int _putchar(char c)
 		return (write(1, &c, 1));
 }
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i;
 	int destl = 0;
 	int srcl = 0;
+	int i;
 
-	while (src[srcl] != '\0')
-	{
-		srcl++;
-	}
 	while (dest[destl] != '\0')
 	{
 		destl++;
 	}
-	for (i = 0; i <= srcl; i++)
+	while (src[srcl] != '\0')
+	{
+		srcl++;
+	}
+	for(i = 0; i < n; i++)
 	{
 		dest[destl + i] = src[i];
 	}
