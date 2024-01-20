f6ff5d20cec1c6f8a772968e0e32147a136ba3c5
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-20 17:44:15 +0200

    parameter

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,3 +5,17 @@
  * @s: parameter of function
  * Return: s value
 */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
