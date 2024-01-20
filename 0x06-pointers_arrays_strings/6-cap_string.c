fd24536ea25d96bc70659e4c559670955ac8d073
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-20 18:15:30 +0200

    comma

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - function to capitalize
+ * @s: first parameter
+ * Return: return s value
+*/
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char sp[13] = {' ', '\n', '\t', ',', ';', '.'}
+	{'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sp[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -3,14 +3,14 @@
 #include <string.h>
 
 int _putchar(char c);
-char *string_toupper(char *s);
+char *cap_string(char *s);
 
 int main(void)
 {
-    char str[] = "Look up!\n";
+char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
     char *ptr;
 
-    ptr = string_toupper(str);
+    ptr = cap_string(str);
     printf("%s", ptr);
     printf("%s", str);
     return (0);
@@ -21,15 +21,26 @@ int _putchar(char c)
 		return (write(1, &c, 1));
 }
 
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
-    int i;
+    int i, j;
+    char sp[13] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
     for (i = 0; s[i] != '\0'; i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z')
+        if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
         {
             s[i] = s[i] - 32;
+        }
+        for (j = 0; j < 13; j++)
+        {
+            if (s[i] == sp[j])
+            {
+                if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+                {
+                    s[i + 1] = s[i + 1] - 32;
+                }
+            }
         }
     }
     return (s);
