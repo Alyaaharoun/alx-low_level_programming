d2a7e126ec37aea305edd306eb205e1515986c13
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-20 17:12:29 +0200

    aass

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -3,16 +3,32 @@
 #include <string.h>
 
 int _putchar(char c);
-int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+
+void print_array(int *a, int n)
+{
+    int i;
+
+    i = 0;
+    while (i < n)
+    {
+        if (i != 0)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+        i++;
+    }
+    printf("\n");
+}
+
 int main(void)
 {
-	
-	char s1[] = "Hello";
-    char s2[] = "World!";
+int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
+    print_array(a, sizeof(a) / sizeof(int));
+    reverse_array(a, sizeof(a) / sizeof(int));
+    print_array(a, sizeof(a) / sizeof(int));
     return (0);
 }
 
@@ -21,19 +37,15 @@ int _putchar(char c)
 		return (write(1, &c, 1));
 }
 
-int _strcmp(char *s1, char *s2)
+void reverse_array(int *a, int n)
 {
-	int i;
+    int i;
+    int j;
 
-    for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+    for (i = 0; i < n / 2; i++)
     {
-        if (s1[i] != s2[i])
-        {
-            if (s1[i] < s2[i])
-                return (s1[i] - s2[i]);
-            else if (s1[i] > s2[i])
-                return (s1[i] - s2[i]);
-        }
+        j = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = j;
     }
-    return (0);
 }
