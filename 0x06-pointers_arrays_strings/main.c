f8aa313c24f9bbf75d7b9e08cbc24d4c6b00c9f5
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-20 21:48:15 +0200

    fnjdndh

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -3,16 +3,16 @@
 #include <string.h>
 
 int _putchar(char c);
-char *cap_string(char *s);
+char *leet(char *s);
 
 int main(void)
 {
-char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
+char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
 
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
     return (0);
 }
 
@@ -21,25 +21,19 @@ int _putchar(char c)
 		return (write(1, &c, 1));
 }
 
-char *cap_string(char *s)
+char *leet(char *s)
 {
     int i, j;
-    char sp[13] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+    char *a = "aAeEoOtTlL";
+    char *b = "4433007711";
 
-    for (i = 0; s[i] != '\0'; i++)
+    for (i = 0; s[i] != '0'; i++)
     {
-        if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+        for (j = 0; j < 10; j++)
         {
-            s[i] = s[i] - 32;
-        }
-        for (j = 0; j < 13; j++)
-        {
-            if (s[i] == sp[j])
+            if (s[i] == a[j])
             {
-                if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-                {
-                    s[i + 1] = s[i + 1] - 32;
-                }
+                s[i] = b[j];
             }
         }
     }
