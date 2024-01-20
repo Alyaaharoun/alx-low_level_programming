53a151b0f9227dc7af0eddff64a224943af2ed75
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-20 16:37:00 +0200

    betty

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -3,39 +3,16 @@
 #include <string.h>
 
 int _putchar(char c);
-char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 int main(void)
 {
 	
-	char s1[98];
-    char *ptr;
-    int i;
+	char s1[] = "Hello";
+    char s2[] = "World!";
 
-    for (i = 0; i < 98 - 1; i++)
-    {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", s1[i]);
-    }
-    printf("\n");
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
     return (0);
 }
 
@@ -44,18 +21,19 @@ int _putchar(char c)
 		return (write(1, &c, 1));
 }
 
-char *_strncpy(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-	return (dest);
+    for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            if (s1[i] < s2[i])
+                return (s1[i] - s2[i]);
+            else if (s1[i] > s2[i])
+                return (s1[i] - s2[i]);
+        }
+    }
+    return (0);
 }
