becaeb8611e624d188451a32c53fbdf374aa2dbc
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-20 22:13:52 +0200

    saas

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "min.h"
+
+/**
+ * leet - function to replace charcter
+ * @s: first parameter
+ * Return: value of s
+*/
+
+char *leet(char *s)
+{
+	int i, j;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
