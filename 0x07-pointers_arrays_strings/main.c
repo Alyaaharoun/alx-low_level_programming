3d84c47e4b9bd60fce9d7136a5ffc42571e64ed9
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-21 03:51:18 +0200

    copy

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -3,37 +3,21 @@
 #include <string.h>
 
 int _putchar(char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-        unsigned int i;
+char *_strchr(char *s, char c);
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
+
 
 int main(void)
 {
-char buffer[98] = {0};
-    char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+char *s = "hello";
+    char *f;
 
-    simple_print_buffer(buffer, 98);
-    _memcpy(buffer + 50, buffer2, 10);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
+    f = _strchr(s, 'l');
+
+    if (f != NULL)
+    {
+        printf("%s\n", f);
+    }
     return (0);
 }
 
@@ -41,13 +25,19 @@ int _putchar(char c)
 {
 		return (write(1, &c, 1));
 }
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_strchr(char *s, char c)
 {
-	unsigned int i;
-
-	for(i = 0; i < n; i++)
+	while (*s != '\0')
 	{
-		dest[i] = src[i];
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
 	}
-	return (dest);
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (NULL);
 }
