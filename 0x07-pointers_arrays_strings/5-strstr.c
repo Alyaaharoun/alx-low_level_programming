e90388daf2767dfd15a379f44c37fe044ced66ff
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-21 05:41:26 +0200

    white space

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,3 +6,29 @@
  * @needle: second parameter
  * Return: value of a pointer
 */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+
+	return (NULL);
+}
