32e100380762967fa5af3bb5d5c8223ef16fab85
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-21 06:03:34 +0200

    weerr

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,5 +4,19 @@
  * print_diagsums - function to sum
  * @a: first parameter
  * @size: second parameter
- * Return: no return
 */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", s1);
+	printf("%d\n", s2);
+}
