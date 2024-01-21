a6bdf0d2ee6e5b6a31a23253496e342b3daf7d34
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-21 02:33:41 +0200

    sssfd

---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
+
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+int main(void)
+{
+char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
+    return (0);
+}
+
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
