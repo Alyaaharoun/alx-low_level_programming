53472e120b3ef02b6525a84d7bb54466b8767309
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-23 16:09:32 +0200

    main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry function
+ * @argc: first parameter
+ * @argv: second prameter
+ * Return: always 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(argc);
+	return (0);
+}
