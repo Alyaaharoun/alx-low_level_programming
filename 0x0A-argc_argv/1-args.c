b8e6e1f81a728a9c411f3e8cb80433cb73842bbc
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-23 16:42:58 +0200

    ssss

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function to print argument
+ * @argc: first parameter
+ * @argv: second parameter
+ * Return: always 0(success)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
