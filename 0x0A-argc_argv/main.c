6069c91e96fdd6354ca05eb6e457806f296f8c88
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-25 13:36:38 +0200

    sddd

--- 0x0A-argc_argv/main.c
+++ 0x0A-argc_argv/main.c
@@ -1,40 +1,28 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
-int atoi(char *s);
+/**
+ * main - function to multiply two numbers
+ * @argc: first parameter
+ * @argv: second parameter
+ * Return: always 0(success)
+*/
+
 int main(int argc, char *argv[])
 {
 	int mul, a, b;
-	mul = a * b;
+
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
+	mul = a * b;
 
-if (argc == 3)
-{
-	printf("%d\n", mul);
-}
-else if (argc != 3)
+if (argc > 3 || argc < 3)
 {
 	printf("Error\n");
 	return (1);
 }
-return (0);
-}
-int atoi(char *s)
-{
-	int res = 0;
-	int sign = 1;
-	int i = 0;
+	printf("%d\n", mul);
 
-	if (s[0] = '-')
-	{
-		sign = -1;
-		i++;
-	}
-	for (; i != '\0'; i++)
-	{
-		res = res * 10 + s[i] - '\0';
-	}
-	return (res * sign);
+return (0);
 }
