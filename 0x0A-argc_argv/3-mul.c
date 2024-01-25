cc6ccc06f8504e1993ab30e46706758137322c9a
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-25 13:40:56 +0200

    aaa

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,15 +13,17 @@ int main(int argc, char *argv[])
 {
 	int mul, a, b;
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	mul = a * b;
-
-if (argc > 3 || argc < 3)
+	if (argc > 3 || argc < 3)
 {
 	printf("Error\n");
 	return (1);
 }
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+
+
 	printf("%d\n", mul);
 
 return (0);
