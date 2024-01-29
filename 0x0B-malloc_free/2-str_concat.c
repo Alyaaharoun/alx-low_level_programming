488fa3887724027044a6c0485666468559f30410
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-01-29 18:01:18 +0200

    dddsdffs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,38 +16,27 @@ char *str_concat(char *s1, char *s2)
 	int l2;
 	int l;
 
-l1 = l2 = 0;
-if (s1 != NULL)
+	l1 = l2 = 0;
+	if (s1 != NULL)
 	{
 		i = 0;
 		while (s1[i++] != '\0')
-		{
 			l1++;
-		}
 	}
 	if (s2 != NULL)
 	{
 		i = 0;
 		while (s2[i++] != '\0')
-		{
 			l2++;
-		}
 	}
 	l = l1 + l2;
-	s = (char*) malloc(sizeof(char) * (l + 1));
+	s = (char *) malloc(sizeof(char) * (l + 1));
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < l1; i++)
-	{
 		s[i] = s1[i];
-	}
 	for (j = 0; j < l2; j++, i++)
-	{
 		s[i] = s2[j];
-	}
 	s[l] = '\0';
 	return (s);
-
 }
