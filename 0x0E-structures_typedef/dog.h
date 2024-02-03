b499133d6c9b9f71be144cf57e13e05806e89899
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-02-04 01:57:52 +0200

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - adog stract
+ * @name: adog name
+ * @age: adog age
+ * @owner: adog owner
+ *
+ * Description: astruct of dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
