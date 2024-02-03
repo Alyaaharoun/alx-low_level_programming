209d69039089caff2fdb020cbb564331dbc94ddc
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-02-03 17:15:51 +0200

    wwwe

--- 0x0D-preprocessor/main.c
+++ 0x0D-preprocessor/main.c
@@ -1,7 +1,7 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "1-pi.h"
 #include <stdio.h>
-#define SIZE 1024
+
 /**
  * main - check the code
  *
@@ -9,9 +9,11 @@
  */
 int main(void)
 {
-    int s;
+    float a;
+    float r;
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
     return (0);
 }
