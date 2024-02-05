a31741b27f0450f52025ef00598b83e56c6ea0bd
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-02-05 16:48:05 +0200

    ssss

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -6,21 +6,14 @@
  *
  * Return: Always 0.
  */
+typedef struct dog  dog_t;
 int main(void)
 {
-struct dog my_dog;
+dog_t my_dog;
 
     my_dog.name = "Poppy";
     my_dog.age = 3.5;
     my_dog.owner = "Bob";
-    print_dog(&my_dog);
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
     return (0);
-}
-void print_dog(struct dog *d)
-{
-	if (d == NULL)
-		return;
-	printf("Name: %s\n", d->name ? d->name : "(nil)");
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
