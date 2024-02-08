6cf492c939c033238d64c7195d96f800fa101143
Alyaaharoun <alyaa.haroun95@gmail.com>
2024-02-08 15:28:41 +0200

    errer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function to print array
+ * @array: first parameter
+ * @size: second parameter
+ * @action: third parameter
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL && action != NULL)
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+}
